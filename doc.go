19020a43d5bf63fcd1eb178a27fc6867c454a7ec
Rohith <gambol99@gmail.com>
2016-05-04 13:51:47 +0100

    - updating the authors - shifting to version v1.0.5

--- doc.go
+++ doc.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	prog        = "keycloak-proxy"
-	version     = "v1.0.4"
+	version     = "v1.0.5"
 	author      = "Rohith"
 	email       = "gambol99@gmail.com"
 	description = "is a proxy using the keycloak service for auth and authorization"
