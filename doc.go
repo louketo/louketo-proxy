2775ff5f5da8d8e7d8f161b63395bd8df0ce2719
Rohith <gambol99@gmail.com>
2018-03-02 21:46:07 +0000

    Spelling Check
    - adding the misspell check to `make test`

--- doc.go
+++ doc.go
@@ -140,7 +140,7 @@ type Config struct {
 	Headers map[string]string `json:"headers" yaml:"headers" usage:"custom headers to the upstream request, key=value"`
 
 	// EnableDefaultDeny indicates we should deny by default all requests
-	EnableDefaultDeny bool `json:"enable-default-deny" yaml:"enable-default-deny" usage:"enables a default denial on all requests, you have to explicity say what is permitted (recommended)"`
+	EnableDefaultDeny bool `json:"enable-default-deny" yaml:"enable-default-deny" usage:"enables a default denial on all requests, you have to explicitly say what is permitted (recommended)"`
 	// EnableEncryptedToken indicates the access token should be encoded
 	EnableEncryptedToken bool `json:"enable-encrypted-token" yaml:"enable-encrypted-token" usage:"enable encryption for the access tokens"`
 	// EnableLogging indicates if we should log all the requests
