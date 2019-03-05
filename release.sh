382f4e052a0d56e551a36a5b7f2e37893a3f0e12
stianst <stianst@gmail.com>
2019-03-05 11:42:56 +0100

    Added release script

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+DIR="$PWD"
+VERSION=`./get-version.sh`
+echo "Version: $VERSION"
+
+TMP=`mktemp -d`
+export GOPATH=$TMP/go
+
+mkdir -p $GOPATH/src/github.com/keycloak
+ln -s $DIR $GOPATH/src/github.com/keycloak/keycloak-gatekeeper
+cd $GOPATH/src/github.com/keycloak/keycloak-gatekeeper
+
+echo "------------------------------------------------------------------------------------------------------------"
+echo "Building:"
+echo ""
+
+make release --trace
+
+
+echo "------------------------------------------------------------------------------------------------------------"
+echo "Upload to jboss.org:"
+echo ""
+
+rsync -rv --protocol=28 $RELDIR/keycloak-gatekeeper/release/* keycloak@filemgmt.jboss.org:/downloads_htdocs/keycloak/$VERSION/gatekeeper/
+
+
+echo "------------------------------------------------------------------------------------------------------------"
+echo "Done"
+echo "------------------------------------------------------------------------------------------------------------"
