1b5dce58eb045c1d88dcfe7dc01d26b320610a18
Stian Thorgersen <stianst@gmail.com>
2019-03-06 13:07:25 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -22,7 +22,7 @@ echo "--------------------------------------------------------------------------
 echo "Upload to jboss.org:"
 echo ""
 
-#rsync -rv --protocol=28 $RELDIR/keycloak-gatekeeper/release/* keycloak@filemgmt.jboss.org:/downloads_htdocs/keycloak/$VERSION/gatekeeper/
+rsync -rv --protocol=28 $RELDIR/keycloak-gatekeeper/release/* keycloak@filemgmt.jboss.org:/downloads_htdocs/keycloak/$VERSION/gatekeeper/
 
 
 echo "------------------------------------------------------------------------------------------------------------"
