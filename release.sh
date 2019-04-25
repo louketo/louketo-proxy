338bb01a523b92b856a287c4f9a1ccf3ae593d68
Bruno Oliveira da Silva <bruno@abstractj.org>
2019-04-25 12:16:59 -0300

    [KEYCLOAK-10121] Keycloak Gatekeeper archives are empty since v6.0.0

--- release.sh
+++ release.sh
@@ -5,11 +5,6 @@ VERSION=`./get-version.sh`
 echo "Version: $VERSION"
 
 TMP=`mktemp -d`
-export GOPATH=$TMP/go
-
-mkdir -p $GOPATH/src/github.com/keycloak
-ln -s $DIR $GOPATH/src/github.com/keycloak/keycloak-gatekeeper
-cd $GOPATH/src/github.com/keycloak/keycloak-gatekeeper
 
 echo "------------------------------------------------------------------------------------------------------------"
 echo "Building:"
@@ -23,7 +18,6 @@ echo "Upload to jboss.org:"
 echo ""
 
 rsync -rv --protocol=28 $DIR/release/* keycloak@filemgmt.jboss.org:/downloads_htdocs/keycloak/$VERSION/gatekeeper/
-
 
 echo "------------------------------------------------------------------------------------------------------------"
 echo "Done"
