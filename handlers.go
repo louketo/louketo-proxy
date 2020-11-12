fe7edeefca3cc8b7a75e8df76b6248b3daac27b6
Pavol Ipoth <pavol.ipoth@telekom.com>
2020-11-12 16:53:35 +0100

    Verify id token not access token

--- handlers.go
+++ handlers.go
@@ -139,19 +139,19 @@ func (r *oauthProxy) oauthCallbackHandler(w http.ResponseWriter, req *http.Reque
 		return
 	}
 
+	// step: check the id token is valid
+	if err = verifyToken(r.client, token); err != nil {
+		r.log.Error("unable to verify the id token", zap.Error(err))
+		r.accessForbidden(w, req)
+		return
+	}
+
 	access, id, err := parseToken(resp.AccessToken)
 	if err == nil {
 		token = access
 		identity = id
 	} else {
 		r.log.Warn("unable to parse the access token, using id token only", zap.Error(err))
-	}
-
-	// step: check the access token is valid
-	if err = verifyToken(r.client, token); err != nil {
-		r.log.Error("unable to verify the id token", zap.Error(err))
-		r.accessForbidden(w, req)
-		return
 	}
 	accessToken := token.Encode()
 
